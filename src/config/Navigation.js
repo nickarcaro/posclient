63da79fdd4f0dff6e3a46b96b53eaf51c7b05977
nicolasaraya77 <nicolas.arayac@mail.udp.cl>
2021-06-04 12:37:33 -0400

    add hashrouter

--- src/config/Navigation.js
+++ src/config/Navigation.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import { map } from "lodash";
 import routes from "./routes";
 
